3aaf9ae90568201dacf7f0baa572282af6d04efc
Plasnid <stuart@sackler.ca>
2022-02-18 11:22:29 -0500

    updated story

--- script.js
+++ script.js
@@ -59,13 +59,53 @@ console.log(steve.releaseTheKraken("Violent Space Wombats",30));
  * * isInsane: true 
  */
 
-/**
- * * Story Notes
- * * trying to save steves life...but he is immortal....the story is about how steve became immortal
- * * steve is not immortal yet, but is going to be because of the actions of Captain Jack Squirrel
- */
+let captainJack = {
+    desc:"Slightly loony academic with a habit of getting into bad situations due to being impulsive.",
+    name:"Captain Jack Squirrel",
+    gender:"m",
+    age:35,
+    backpackContents: ["mixed nuts", "chocolate", "pipe", "rum", "book about the legend of the immortal portal"],
+    // * steal lunchboxes
+    // * dropping candle in library
+    // * driving(poorly)
+    clumsyTheft: function(target, fatigue){
+        // * early morning most alert 90-100
+        // * too much rum 10-20
+        // * really really tired 0-10
+        let consequences = "";
+        /**
+         * * < less than
+         * * > greater than
+         * * >= greater than or equal to
+         * * <= less than or equal to
+         * * == equal to
+         * * != not equal to
+         * 
+         * * "" '' ``
+         */
+        if(fatigue>=0 && fatigue<=10){
+            consequences = `he trips over a random ball and drops the ${target} to the floor`;
+        }else if(fatigue>10 && fatigue<=20){
+            consequences = `Captain Jack Squirrel goes to get the${target} but hits their head on the bookshelf which then falls on them.`
+        }else{
+            consequences = `Tries to grab ${target} but misses and slams hand into what they seem to actually need by mistake.`;
+        }
+        return consequences;
+    }
+}
 
-
+function showStory(displayStr){
+    // * grab the body tag
+    let theBody = document.querySelector("body");
+    // * create a p tag
+    let storyElement =  document.createElement("p");
+    // * add display string to the p tag
+    storyElement.innerText = displayStr;
+    // * append it to the body tag and to the console
+    // * parent.appendChild(elementToAdd)
+    theBody.appendChild(storyElement);
+    console.log(displayStr);
+}
 
 function showTitle(titleStr){
     console.log(titleStr);
@@ -80,3 +120,21 @@ function showTitle(titleStr){
 }
 let storyTitle = "The Immortal Portal";
 showTitle(storyTitle);
+
+/**
+ * * Story Notes
+ * * trying to save steves life...but he is immortal....the story is about how steve became immortal
+ * * steve is not immortal yet, but is going to be because of the actions of Captain Jack Squirrel
+ */
+
+showStory(`${steve.name} and ${captainJack.name} are in the library`);
+showStory(`${steve.name} reaches into their backpack and pulls out a kitkat bar`);
+showStory(`${steve.name} evilly takes a bite out of the side of the kitkat bar!`);
+showStory(`${steve.name} feels a crazy electric shock as he bites into the bar!`);
+showStory(`${captainJack.name} wakes up, sees the kitkat bar and plots to steal it!`);
+showStory("The librarian points him in the direction of steve but advises him to avoid Steve due to his eating KitKat bars without breaking them.");
+showStory(captainJack.clumsyTheft("kitkat bar", 3));
+showStory(`${captainJack.name} realizes the bar is made of beans and tries to avoid the candle as he is falling to the floor.`);
+showStory(`At that moment ${steve.name} feels a ripper about to emerge!`);
+showStory(steve.releaseTheKraken("Steve",100));
+showStory(`${steve.name} has opened the immortal portal and gained its immortality powers...but the library is ruined`);
